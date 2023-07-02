b8475f379034db2d298e867fe2f52bc03916bf56
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-07-02 20:02:36 +0800

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,50 @@
+#Completely Randomized Design (CRD):
+import pandas as pd
+import statsmodels.api as sm
+# Read data from CSV
+data = pd.read_csv('crd_data.csv')
+# Perform analysis
+model = sm.formula.ols('Yield ~ Treatment', data=data).fit()
+print(model.summary())
+
+#Randomized Complete Block Design (RCBD):
+
+import pandas as pd
+import statsmodels.api as sm
+# Read data from CSV
+data = pd.read_csv('rcbd_data.csv')
+# Perform analysis
+model = sm.formula.ols('Yield ~ Treatment + Block', data=data).fit()
+print(model.summary())
+
+#Split-Plot Design:
+
+import pandas as pd
+import statsmodels.api as sm
+# Read data from CSV
+data = pd.read_csv('split_plot_data.csv')
+# Perform analysis
+model = sm.formula.ols('Yield ~ Treatment + WholePlot + SubPlot', data=data).fit()
+print(model.summary())
+
+#Factorial Design:
+
+import pandas as pd
+import statsmodels.api as sm
+# Read data from CSV
+data = pd.read_csv('factorial_data.csv')
+# Perform analysis
+model = sm.formula.ols('Yield ~ Factor1 * Factor2', data=data).fit()
+print(model.summary())
+
+#Latin Square Design:
+
+import pandas as pd
+import statsmodels.api as sm
+# Read data from CSV
+data = pd.read_csv('latin_square_data.csv')
+# Perform analysis
+model = sm.formula.ols('Yield ~ Row + Column', data=data).fit()
+print(model.summary())
+
+
